0b4f12c8ae8326cacf6086bfa06c5ee8c57b59e2
djrmarques <31391588+djrmarques@users.noreply.github.com>
2018-01-20 12:38:49 -0800

    New query - Replace ID

--- mysql/queries/7-replaceid.sql
+++ mysql/queries/7-replaceid.sql
@@ -1,10 +1,11 @@
-select Plant
-from(
-
-select p.Name as 'Plant', f.Name as 'Farm', w.Location as 'Warehouse',
-pp.Amount as 'Amount', pp.UnitPrice as 'Unit Price', pp.Date as 'Entry Date'
-from Plant as p, Farm as f, Warehouse as w, Produced as pp
-where p.IDPlant = pp.IDPlant 
-and f.IDFarm = pp.IDFarm
-and w.IDWarehouse = pp.IDWarehouse
-and pp.Amount > 30) as t
+select *  from 
+(
+	select p.Name as 'Plant', f.Name as 'Farm', w.Location as 'Warehouse',
+	pp.Amount as 'Amount', pp.UnitPrice as 'Unit Price', pp.Date as 'Entry Date'
+	from Plant as p, Farm as f, Warehouse as w, Produced as pp
+	where p.IDPlant = pp.IDPlant 
+	and f.IDFarm = pp.IDFarm
+	and w.IDWarehouse = pp.IDWarehouse
+) as R
+where Warehouse != 'New'
+-- and pp.Amount > 30
