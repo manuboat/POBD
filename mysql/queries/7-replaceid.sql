88599863307f69171f3207a4803d3c7181462a0f
djrmarques <31391588+djrmarques@users.noreply.github.com>
2018-01-22 05:21:58 -0800

    Added ID

--- mysql/queries/7-replaceid.sql
+++ mysql/queries/7-replaceid.sql
@@ -1,11 +1,13 @@
+
+
 select *  from 
 (
-	select p.Name as 'Plant', f.Name as 'Farm', w.Location as 'Warehouse',
+	select p.IDPlant as 'ID' ,p.Name as 'Plant', f.Name as 'Farm', w.Location as 'Warehouse',
 	pp.Amount as 'Amount', pp.UnitPrice as 'Unit Price', pp.Date as 'Entry Date'
 	from Plant as p, Farm as f, Warehouse as w, Produced as pp
 	where p.IDPlant = pp.IDPlant 
 	and f.IDFarm = pp.IDFarm
 	and w.IDWarehouse = pp.IDWarehouse
 ) as R
-where Warehouse != 'New'
+where Warehouse = 'New'
 -- and pp.Amount > 30
